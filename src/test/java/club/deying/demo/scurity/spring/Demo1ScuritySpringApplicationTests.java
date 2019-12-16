9debec2721ffc1ba7f9c3c1f6100daa997934dbb
dengdeying <dengdeying1980@gmail.com>
2019-12-16 09:56:45 +0800

    init

---
+++ src/test/java/club/deying/demo/scurity/spring/Demo1ScuritySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package club.deying.demo.scurity.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ScuritySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
